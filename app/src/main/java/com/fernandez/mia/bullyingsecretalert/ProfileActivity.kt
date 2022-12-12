03f03644b3ae7db2e2644f00b341b91c890934cb
miafernandez14 <mia.fernandez@tecsup.edu.pe>
2022-12-12 10:23:24 -0500

    5 commit

--- app/src/main/java/com/fernandez/mia/bullyingsecretalert/ProfileActivity.kt
+++ app/src/main/java/com/fernandez/mia/bullyingsecretalert/ProfileActivity.kt
@@ -2,10 +2,15 @@ package com.fernandez.mia.bullyingsecretalert
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fernandez.mia.bullyingsecretalert.databinding.ActivityMainBinding
+import com.fernandez.mia.bullyingsecretalert.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val extras = this.intent.extras
     }
 }
